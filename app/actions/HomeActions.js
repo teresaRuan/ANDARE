d0ec5078873ae8fc6a68a92cb0bd6e9bdbb9a371
teresaRuan <564328091@qq.com>
2016-12-25 15:21:33 +0800

    暂时删除

--- app/actions/HomeActions.js
+++
@@ -1,7 +0,0 @@
-import {FETCH_ARTICLE} from '../constants/ActionTypes'
-import {initState} from '../test/date'
-export function getList(argument) {
-    return{
-        type:FETCH_ARTICLE
-    }
-}
--- app/actions/squareAction.js
+++ app/actions/squareAction.js
@@ -1,15 +1,1 @@
-import * as types from '../constants/ActionTypes';
-
-export function sendMessage(message) {
-    return {
-        type: types.SEND_MESSAGE,
-        message
-    }
-}
-
-export function receiveMessage(message) {
-    return {
-        type: types.RECEIVE_MESSAGE,
-        message
-    }
-}
+s
